83e9f94d02a7fbd0b16a14df2861b5c32ce64839
mayowa.falade@andela.co <andela@Andela-Devss-MacBook-Pro.local>
2015-03-28 19:23:46 +0100

    Quiz page and its lil transition animations, centering of the homepage button

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,11 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		})
+
+		.state('quiz', {
+			url: '/quiz',
+			templateUrl: 'modules/core/views/quiz.takequiz.view.html'
 		});
 	}
 ]);
